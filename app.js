ed4f7a2e1981b4dddc58dcaee9954192bd3d329c
Phuong Thanh <phuong74200@gmail.com>
2022-08-07 01:21:46 +0700

    change

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ db.page.loadDatabase();
 app.use(express.static('public'));
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const image = multer({
     dest: 'images/',
     fileFilter: (req, file, cb) => {
